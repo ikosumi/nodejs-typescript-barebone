8dac9763bca03d7b18127f99870bfb12826b1cd3
Ilir Kosumi <64077312+ikosumi@users.noreply.github.com>
2025-07-02 10:03:45 +0200

    Only transpile files under src

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export * from './app';
+import { AppConfig } from './types';
+import App from './app';
+
+const config: AppConfig = {
+  name: 'users',
+  version: '1.0',
+  environment: 'development',
+};
+
+const app = new App(config);
+
+app.start();
